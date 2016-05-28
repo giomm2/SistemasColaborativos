7aff80ab0000e4cc51341de6beef79ce351a1035
giomm2 <giomm269@gmail.com>
2016-05-28 09:58:25 -0600

    Se incluyen pruebas funcionales.

---
+++ Calculadora.Tests/Sumas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logica.Negocio;
+
+namespace Calculadora.Tests
+{
+    [TestClass]
+    public class Sumas
+    {
+        [TestMethod]
+        public void SumarUnoMasDos()
+        {
+            //Paso 1: Defina el escenario de la prueba
+            var elPrimerOperando = 1;
+            var elSegundoOperando = 2;
+            var elResultadoEsperado = 3;
+
+            //Paso 2: Invoque al metodo que se desea validar.
+            var laSuma = new Suma();
+            var elResultado = laSuma.Operar(elPrimerOperando, elSegundoOperando);
+
+            //Paso 3: Verificar o comparar el resltado obtenido con el esperado.
+            Assert.AreEqual(elResultadoEsperado,elResultado);
+
+
+        }
+    }
+}
